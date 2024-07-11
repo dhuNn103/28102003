4e871e9d710a1c3619f3cc94ca242efe9551e7d5
dhuNn103 <la123este@gmail.com>
2024-07-11 23:54:49 +0700

    header_Done

--- fe/heahaoishop/src/page/users/theme/header/index.js
+++ fe/heahaoishop/src/page/users/theme/header/index.js
@@ -1,10 +1,70 @@
 import { memo } from "react";
+import "./style.scss";
+import {
+  FaInstagram,
+  FaFacebookF,
+  FaGoogle,
+  FaLinkedinIn,
+  FaTwitter,
+  FaRegUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { MdAttachEmail } from "react-icons/md";
+import { formatter } from "utils/fomater";
 
 const Header = () => {
   return (
-    
-      <h1>Header</h1>
+    <div className="header_top">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 header_top_left">
+            <ul>
+              <li>
+                <MdAttachEmail />
+                heloo@gmail.com
+              </li>
 
+              <li>freeShip {formatter(300000)}</li>
+            </ul>
+          </div>
+          <div className="col-6 header_top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <FaFacebookF />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaGoogle />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaLinkedinIn />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaRegUser />
+                </Link>
+                <span>Login</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
