0ee9af40bda6026529b5cd4eaa5d695c15e48d9e
dhuNn103 <la123este@gmail.com>
2024-07-25 10:50:15 +0700

    done

--- fe/heahaoishop/src/page/users/theme/header/index.js
+++ fe/heahaoishop/src/page/users/theme/header/index.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss";
 import {
   FaInstagram,
@@ -7,64 +7,159 @@ import {
   FaLinkedinIn,
   FaTwitter,
   FaRegUser,
+  FaShoppingCart,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { MdAttachEmail } from "react-icons/md";
 import { formatter } from "utils/fomater";
+import { ROUTERS } from "utils/router";
 
 const Header = () => {
-  return (
-    <div className="header_top">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 header_top_left">
-            <ul>
-              <li>
-                <MdAttachEmail />
-                heloo@gmail.com
-              </li>
+  const [menus, setMenus] = useState([
+    {
+      name: "Trang chủ",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Cửa hàng",
+      path: ROUTERS.USER.PRODUCT,
+    },
+    {
+      name: "Sản phẩm",
+      path: "",
+      isShowSubmenu: false,
+      child: [
+        {
+          name: "Thịt",
+          path: "",
+        },
+        {
+          name: "Rau củ",
+          path: "",
+        },
+        {
+          name: "Thức ăn nhanh",
+          path: "",
+        },
+        {
+          name: "Trứng",
+          path: "",
+        },
+      ],
+    },
+    {
+      name: "Giới thiệu",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "bài viết",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Liên hệ",
+      path: ROUTERS.USER.HOME,
+    },
+  ]);
 
-              <li>freeShip {formatter(300000)}</li>
-            </ul>
-          </div>
-          <div className="col-6 header_top_right">
-            <ul>
-              <li>
-                <Link to={""}>
-                  <FaFacebookF />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <FaInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <FaGoogle />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <FaLinkedinIn />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <FaTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <FaRegUser />
-                </Link>
-                <span>Login</span>
-              </li>
-            </ul>
+  return (
+    <>
+      <div className="header_top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header_top_left">
+              <ul>
+                <li>
+                  <MdAttachEmail />
+                  heloo@gmail.com
+                </li>
+
+                <li>freeShip {formatter(300000)}</li>
+              </ul>
+            </div>
+            <div className="col-6 header_top_right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <FaFacebookF />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <FaInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <FaGoogle />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <FaLinkedinIn />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <FaTwitter />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <FaRegUser />
+                  </Link>
+                  <span>Login</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-3">
+            <div className="header_logo">
+              <h1>HeaHaoi</h1>
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <nav className="header_menu">
+              <ul>
+                {menus?.map((menu, menuKey) => (
+                  <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                    <Link to={menu?.path}>{menu?.name}</Link>
+                  </li>
+                ))}
+
+                {/* <li>Giới thiệu</li>
+                <li>
+                  <ul>
+                    <li>Thịt</li>
+                    <li>Thịt</li>
+                    <li>Thịt</li>
+                  </ul>
+                </li>
+                <li>Liên hệ</li> */}
+              </ul>
+            </nav>
+          </div>
+          <div className="col-xl-3">
+            <div className="header_cart">
+              <div className="header_cart_price">
+                <span>{formatter(10)}</span>
+              </div>
+              <ul>
+                <li>
+                  <Link to={"#"}>
+                    <FaShoppingCart />
+                    <span>5</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
